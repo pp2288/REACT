9ecc18ad7d0d4421d2eb5496c66efdbd10636a21
pp2288 <polinkatr@gmail.com>
2020-02-01 17:20:13 +0300

    3

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MessageField from './components/MessageField.jsx';
+import Layout from './components/Layout';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
-   <MessageField/>,
+    <MuiThemeProvider>
+        <Layout />
+   </MuiThemeProvider>,
    document.getElementById('root'),
 );
