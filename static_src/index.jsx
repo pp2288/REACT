5bba7e2b17d56f25f98344c9d20a377ff5a7d900
pp2288 <polinkatr@gmail.com>
2020-02-05 21:58:47 +0300

    3

--- static_src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MessageField from './components/MessageField.jsx';
-
-ReactDOM.render(
-   <MessageField/>,
-   document.getElementById('root'),
-);
